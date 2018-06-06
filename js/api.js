68de9c716200df4c337d28b55efa51b7c4e4ccdd
NEMMICHE Abdel <abdel.nemmiche@pole-emploi.fr>
2018-06-06 18:17:29 +0200

    ajout des mots clé

--- js/api.js
+++ js/api.js
@@ -1,30 +1,17 @@
 $(document).ready(function() {
-    /*$.ajax({
-        url: "http://rest-service.guides.spring.io/greeting",
-        type: 'GET',
-        crossDomain: true
-    }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
-    });*/
     $.ajax({
-      url: "http://rest-service.guides.spring.io/greeting",
-        dataType: "jsonp",
-        crossDomain: true,
-      success: function(data) {
-        alert(data);
-      }
+        url: "http://localhost:8080/acronyms",
+          crossDomain : true,
+          type: "GET"
+    }).then(function(data) {
+       $('.acronym-name').append(data[0].name);
+       $('.acronym-summary').append(data[0].description.shortD);
+       $('.acronym-definition').append(data[0].description.longD);
+       $('.acronym-creation').append(data[0].creation);
+       $('.acronym-author').append(data[0].author);
+       $('.acronym-obsolete').append(data[0].obsolete);
+       $('.acronym-motCle1').append(data[0].motClefs[0]);
+       $('.acronym-motCle2').append(data[0].motClefs[1]);
+       $('.acronym-motCle3').append(data[0].motClefs[2]);
     });
 });
-
-
-// // Ici, la requête sera émise de façon synchrone.
-// const req = new XMLHttpRequest();
-// req.open('GET', 'http://www.mozilla.org/', false);
-// req.send(null);
-//
-// if (req.status === 200) {
-//     console.log("Réponse reçue: %s", req.responseText);
-// } else {
-//     console.log("Status de la réponse: %d (%s)", req.status, req.statusText);
-// }
